52bd90c1d5d81f648c8fefdb9fcefd7ce389ec9c
Umangsharma15 <umangpandittt15@gmail.com>
2026-02-07 10:24:21 +0530

    project structure ready venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Umang Sharma",
+    author_email="umangpandittt15@gmail.com",
+    packages=find_packages()
+)
